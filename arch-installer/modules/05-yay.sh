4204ffb0352b2d8d4d8e853bc318ae14556eeb4b
Haidex3 <haider.rodriguez-m@mail.escuelaing.edu.co>
2026-02-19 21:42:09 -0500

    fix :final yay

--- arch-installer/modules/05-yay.sh
+++ arch-installer/modules/05-yay.sh
@@ -5,12 +5,10 @@ echo "----------------------------------------"
 echo "Installing yay AUR helper"
 echo "----------------------------------------"
 
-# Directorio temporal de build
 BUILD_DIR="/home/$USERNAME/build-yay"
 mkdir -p "$BUILD_DIR"
 chown "$USERNAME:$USERNAME" "$BUILD_DIR"
 
-# Ejecutar build como el usuario normal
 su - "$USERNAME" -c "
 cd $BUILD_DIR &&
 git clone https://aur.archlinux.org/yay.git &&
@@ -18,7 +16,6 @@ cd yay &&
 makepkg -f --noconfirm
 "
 
-# Instalar el paquete generado como root
 PACMAN_PKG=$(find "$BUILD_DIR/yay" -maxdepth 1 -type f -name "yay-*.pkg.tar.zst" | head -n1)
 
 if [ -f "$PACMAN_PKG" ]; then
